6519b5f4aff20f0682716ff5e5662fddabbea3ef
egner henry <egnerhenry@gmail.com>
2021-03-18 09:56:13 -0500

    a

--- application/views/IngresarProducto.php
+++ application/views/IngresarProducto.php
@@ -25,8 +25,8 @@
                                                                 <div class="row">
                                                                     <div class="col-sm-6">
                                                                         <div class="form-group">
-                                                                            <label for="fullName">Nombre el Producto</label>
-                                                                            <input type="text" class="form-control mb-4" id="fullName" placeholder="Full Name" value="Jimmy Turner">
+                                                                            <label for="fullName">Nombre del Producto</label>
+                                                                            <input type="text" class="form-control mb-4" id="fullName" placeholder="Full Name" value="Nombre del Producto">
                                                                         </div>
                                                                     </div>
                                                                     <div class="col-sm-6">
@@ -35,36 +35,9 @@
                                                                             <div class="form-group mr-1">
                                                                                 <select class="form-control" id="exampleFormControlSelect1">
                                                                                   <option>Day</option>
-                                                                                  <option>1</option>
-                                                                                  <option>2</option>
-                                                                                  <option>3</option>
-                                                                                  <option>4</option>
-                                                                                  <option>5</option>
-                                                                                  <option>6</option>
-                                                                                  <option>7</option>
-                                                                                  <option>8</option>
-                                                                                  <option>9</option>
-                                                                                  <option>10</option>
-                                                                                  <option>11</option>
-                                                                                  <option>12</option>
-                                                                                  <option>13</option>
-                                                                                  <option>14</option>
-                                                                                  <option>15</option>
-                                                                                  <option>16</option>
-                                                                                  <option>17</option>
-                                                                                  <option>18</option>
-                                                                                  <option>19</option>
                                                                                   <option selected>20</option>
                                                                                   <option>21</option>
-                                                                                  <option>22</option>
-                                                                                  <option>23</option>
-                                                                                  <option>24</option>
-                                                                                  <option>25</option>
-                                                                                  <option>26</option>
-                                                                                  <option>27</option>
-                                                                                  <option>28</option>
-                                                                                  <option>29</option>
-                                                                                  <option>30</option>
+                                                                                 
                                                                                 </select>
                                                                             </div>
                                                                             <div class="form-group mr-1">
