70ebed5ef7f78508fbf92ef9a5f7c1ff97f204e0
12Alexis <74570955+12Alexis@users.noreply.github.com>
2021-03-24 11:09:55 -0500

    boton editar
    edit

--- application/views/IngresarProducto.php
+++ application/views/IngresarProducto.php
@@ -129,6 +129,8 @@
                                             <td><?php echo $value->prod_medida_id?></td>
                                             <td><?php echo $value->prod_precio_venta?></td>
                                             <td><?php echo $value->prod_stock?></td>
+                                            <td><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></td>
+                                            <td><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg></td>
                                             </tr>
                                       <?php  } ?>
                                     </thead>
